0ac98f5b3446302ea36a8b25fb53759211946c8e
Darya_KRV <darya-korovataya@yandex.ru>
2023-03-06 14:56:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppMyName/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleAppMyName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Darya";
+            Console.WriteLine(MyName);
+            Console.ReadKey();
+        }
+    }
+}
